e3f2f18c169e08e330fca056813a7d64b9035309
Sanjay-2901 <sanjay@yavar.in>
2023-08-09 12:56:13 +0530

    Feat: Auth-components

--- src/app/views/public/authentication/forgot-password/forgot-password.component.ts
+++ src/app/views/public/authentication/forgot-password/forgot-password.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss'],
+  styleUrls: ['../login/login.component.scss', './forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {}
