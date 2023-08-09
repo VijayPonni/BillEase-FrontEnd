5544a81ba893eb2ffb2a14cf9440dd6218fc6ff0
Vijay S <vijay.s@yavar.in>
2023-08-09 11:45:59 +0530

    folder-structure

---
+++ src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { P404Component } from './views/public/error/p404/p404.component';
+import { DefaultLayoutComponent } from './shared/containers/default-layout/default-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./views/public/authentication/authentication.module').then(
+        (module) => module.AuthenticationModule
+      ),
+  },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    loadChildren: () =>
+      import('./views/secure/secure.module').then((module) => module.SecureModule),
+  },
+  {
+    path: '**',
+    component: P404Component,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
