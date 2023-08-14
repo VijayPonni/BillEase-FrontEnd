07f6cbbffd3b948884f7ef195a5049275edac829
Sanjay-2901 <sanjay@yavar.in>
2023-08-14 10:26:01 +0530

    Added animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, P404Component, DefaultLayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, height: '0' }),
+        animate('300ms ease-in', style({ opacity: 1, height: '*' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 0, height: '*' }),
+        animate('300ms ease-out', style({ opacity: 0, height: '0' })),
+      ]),
+    ]),
+  ],
 })
 export class HeaderComponent {
   showDropDown = false;
   showCurrentPassword = false;
   showNewPassword = false;
+  showChangePasswordForm = false;
 
   toggleDropDown(event: MouseEvent): void {
     this.showDropDown = !this.showDropDown;
+    this.showChangePasswordForm = false;
     event.stopPropagation();
   }
 
   closeDropDown(): void {
     if (this.showDropDown) {
       this.showDropDown = false;
+      this.showChangePasswordForm = false;
     }
   }
 
@@ -26,5 +42,9 @@ export class HeaderComponent {
 
   toggleNewPassword(): void {
     this.showNewPassword = !this.showNewPassword;
+  }
+
+  toggleChangePasswordForm(): void {
+    this.showChangePasswordForm = !this.showChangePasswordForm;
   }
 }
